f6e37a4055972dc68fa896749fa7640d536f5299
bonk-del <bryangradi@gmail.com>
2026-04-27 12:25:15 -0300

    Update app.py
    def soma

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-def soma(a,b):
-    return a+b
+def soma(a, b):
+    return a + b
+
+
+def subtrai(a, b):
+    return a - b
+
+
+def multiplica(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Não é possível dividir por zero")
+    return a / b
